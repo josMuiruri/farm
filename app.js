95c196e257792896326e8fc6122dff38a99d0443
josMuiruri <josemuiruri51@gmail.com>
2025-01-22 02:28:05 +0300

    handling GET requests

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/api/v1/products', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: products.length,
+        data: {
+            products
+        }
+    })
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('App running on port ${port}...');
+})
