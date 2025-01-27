378044e692420e0b7121581d4b89c6561c7d3f0c
josMuiruri <josemuiruri51@gmail.com>
2025-01-27 12:54:30 +0300

    handling post requests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/api/v1/products', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -12,7 +14,22 @@ app.get('/api/v1/products', (req, res) => {
     })
 })
 
+app.post('api/v1/products', (req, res) => {
+
+    const newId = products[products.length - 1].id + 1;
+    const newProduct = Object.assign({ id: newId}, req.body);
+
+    products.push(newProduct);
+    res.status(201).json({
+        status: 'success',
+        data: {
+            product: newProduct
+        }
+    })
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log('App running on port ${port}...');
 })
+
