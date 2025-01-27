2d65bd387f402f246cb0308a3d2dc7949a41868c
josMuiruri <josemuiruri51@gmail.com>
2025-01-27 17:34:12 +0300

    responding to URL parameters

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ app.get('/api/v1/products', (req, res) => {
     })
 })
 
+app.get('/api/v1/products/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const product = products.find(el => el.id === id);
+
+    if (!product) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            product
+        }
+    })
+})
+
 app.post('api/v1/products', (req, res) => {
 
     const newId = products[products.length - 1].id + 1;
