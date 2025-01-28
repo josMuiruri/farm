318add56ede170786bac62f3296cc82093544da3
josMuiruri <josemuiruri51@gmail.com>
2025-01-28 13:33:55 +0300

    structuring

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 
-
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // middlewares
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
-
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-})
-
+module.exports = app;
