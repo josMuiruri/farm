d55b3b8aa8d730fa8ff8048d979d9d5ea44342ac
josMuiruri <josemuiruri51@gmail.com>
2025-02-08 11:48:22 +0300

    deleting documents

--- controllers/productController.js
+++ controllers/productController.js
@@ -76,9 +76,18 @@ exports.updateProduct = async (req, res) => {
   }
 };
 
-exports.deleteProduct = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
