a3a6ab2e1976ce690eff5ebdf51df8efc4a7e095
josMuiruri <josemuiruri51@gmail.com>
2025-02-13 03:52:57 +0300

    implemet account creation & deletion

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,54 @@
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+// update current user
+exports.updateMe = async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400,
+      ),
+    );
+  }
+
+  // filter - what to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // update user doc
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updateUser,
+    },
+  });
+};
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please sign up',
+  });
+};
+
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
